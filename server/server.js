55e5d5bbbf6573fedb0d9371f1ecf1302056b388
Mykhailo <mykhailo.vz@gmail.com>
2017-04-18 06:50:47 -0400

    Add POST /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+// save new something
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo')
+// });
+
+// var newTodo2 = new Todo({
+//     text: 'Feed the cat',
+//     completed: true,
+//     completedAt: 123
+// });
+
+// var newTodo2 = new Todo({
+//     text: 'Something to do'
+// });
+
+// newTodo2.save().then((doc) => {
+//     console.log('Save todo #2', doc);
+// }, (e) => {
+//     console.log('Unable to save todo #2', e);
+// });
+
+// User Model
+// email [require, trim, set type, minlength] [explore custom validation]
+// 
+
+// var user = new User({
+//     email: 'andrew@example.com'
+// });
+
+// user.save().then((doc) => {
+//     console.log('User saved', doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
