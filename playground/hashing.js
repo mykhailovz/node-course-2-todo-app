862ed66defd907214a62591211f133292670842f
Mykhailo <mykhailo.vz@gmail.com>
2017-04-20 05:54:15 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -29,17 +29,39 @@ const {SHA256} = require('crypto-js');
 // } else {
 //     console.log('Data was changed. Do not trust');
 // }
-
-const jwt = require('jsonwebtoken');
-
-
-var data = {
-    id: 10
-};
-
-var token = jwt.sign(data, '123abc');
-console.log(token);
+//-------------------------------------------------//
+// const jwt = require('jsonwebtoken');
 
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded ', decoded);
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded ', decoded);
+
+
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$pQ2xls8Lb/FK0HfKxZa4RuMwgU2jRZkuC4NnClqICXvuE2yxae21a';
+
+
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+
